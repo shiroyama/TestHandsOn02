557477de7453045389cb0a835314038207a9bdbf
Fumihiko Shiroyama <fu.shiroyama@gmail.com>
2016-10-28 17:21:04 +0900

    Add answer for LoginActivity

--- app/src/androidTest/java/android/shiroyama/us/testhandson02/LoginActivityTest.java
+++ app/src/androidTest/java/android/shiroyama/us/testhandson02/LoginActivityTest.java
@@ -2,6 +2,7 @@ package android.shiroyama.us.testhandson02;
 
 
 import android.support.design.widget.TextInputLayout;
+import android.support.test.espresso.ViewInteraction;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 import android.test.suitebuilder.annotation.LargeTest;
@@ -15,6 +16,17 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static android.support.test.espresso.action.ViewActions.replaceText;
+import static android.support.test.espresso.action.ViewActions.scrollTo;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withParent;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.allOf;
+
 @LargeTest
 @RunWith(AndroidJUnit4.class)
 public class LoginActivityTest {
@@ -24,27 +36,94 @@ public class LoginActivityTest {
 
     @Test
     public void Eメールパスワードともに空欄でボタンを押しエラーになるテストを書こう() {
+        ViewInteraction button = onView(
+                allOf(withId(R.id.email_sign_in_button), withText("Sign in or register"),
+                        withParent(allOf(withId(R.id.email_login_form),
+                                withParent(withId(R.id.login_form))))));
+        button.perform(scrollTo(), click());
 
+        onView(withId(R.id.email_input_layout)).check(matches(hasTextInputLayoutHintText("This field is required")));
+        onView(withId(R.id.password_input_layout)).check(matches(hasTextInputLayoutHintText("This password is too short")));
     }
 
     @Test
     public void アットマークがない不正なEメールアドレスを入力してエラーになるテストを書こう() {
+        ViewInteraction email = onView(
+                withId(R.id.email));
+        email.perform(scrollTo(), replaceText("foo"), closeSoftKeyboard());
 
+        ViewInteraction password = onView(
+                withId(R.id.password));
+        password.perform(scrollTo(), replaceText("testtest"), closeSoftKeyboard());
+
+        ViewInteraction button = onView(
+                allOf(withId(R.id.email_sign_in_button), withText("Sign in or register"),
+                        withParent(allOf(withId(R.id.email_login_form),
+                                withParent(withId(R.id.login_form))))));
+        button.perform(scrollTo(), click());
+
+        onView(withId(R.id.email_input_layout)).check(matches(hasTextInputLayoutHintText("This email address is invalid")));
+        onView(withId(R.id.password_input_layout)).check(matches(hasTextInputLayoutHintText("")));
     }
 
     @Test
     public void 四文字以下の短いパスワードを入力してエラーになるテストを書こう() {
+        ViewInteraction email = onView(
+                withId(R.id.email));
+        email.perform(scrollTo(), replaceText("foo@example.com"), closeSoftKeyboard());
 
+        ViewInteraction password = onView(
+                withId(R.id.password));
+        password.perform(scrollTo(), replaceText("test"), closeSoftKeyboard());
+
+        ViewInteraction button = onView(
+                allOf(withId(R.id.email_sign_in_button), withText("Sign in or register"),
+                        withParent(allOf(withId(R.id.email_login_form),
+                                withParent(withId(R.id.login_form))))));
+        button.perform(scrollTo(), click());
+
+        onView(withId(R.id.email_input_layout)).check(matches(hasTextInputLayoutHintText("")));
+        onView(withId(R.id.password_input_layout)).check(matches(hasTextInputLayoutHintText("This password is too short")));
     }
 
     @Test
     public void Eメールパスワードともに形式は正しいが情報が間違っているためエラーになるテストを書こう() {
+        ViewInteraction email = onView(
+                withId(R.id.email));
+        email.perform(scrollTo(), replaceText("foo@example.com"), closeSoftKeyboard());
 
+        ViewInteraction password = onView(
+                withId(R.id.password));
+        password.perform(scrollTo(), replaceText("testtest"), closeSoftKeyboard());
+
+        ViewInteraction button = onView(
+                allOf(withId(R.id.email_sign_in_button), withText("Sign in or register"),
+                        withParent(allOf(withId(R.id.email_login_form),
+                                withParent(withId(R.id.login_form))))));
+        button.perform(scrollTo(), click());
+
+        onView(withId(R.id.email_input_layout)).check(matches(hasTextInputLayoutHintText("")));
+        onView(withId(R.id.password_input_layout)).check(matches(hasTextInputLayoutHintText("This password is incorrect")));
     }
 
     @Test
     public void Eメールパスワードともに形式が正しく情報も合っているテストを書こう() {
+        ViewInteraction email = onView(
+                withId(R.id.email));
+        email.perform(scrollTo(), replaceText("foo@example.com"), closeSoftKeyboard());
 
+        ViewInteraction password = onView(
+                withId(R.id.password));
+        password.perform(scrollTo(), replaceText("hello"), closeSoftKeyboard());
+
+        ViewInteraction button = onView(
+                allOf(withId(R.id.email_sign_in_button), withText("Sign in or register"),
+                        withParent(allOf(withId(R.id.email_login_form),
+                                withParent(withId(R.id.login_form))))));
+        button.perform(scrollTo(), click());
+
+        onView(withId(R.id.email_input_layout)).check(matches(hasTextInputLayoutHintText("")));
+        onView(withId(R.id.password_input_layout)).check(matches(hasTextInputLayoutHintText("")));
     }
 
     /**
