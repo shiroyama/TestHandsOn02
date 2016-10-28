9df108769f401e56802b79625a7a091f36ac981f
Fumihiko Shiroyama <fu.shiroyama@gmail.com>
2016-10-28 16:50:23 +0900

    add helloespresso

---
+++ helloespresso/src/androidTest/java/android/shiroyama/us/helloespresso/MainActivityTest.java
@@ -0,0 +1,38 @@
+package android.shiroyama.us.helloespresso;
+
+
+import android.support.test.espresso.ViewInteraction;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import android.test.suitebuilder.annotation.LargeTest;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withParent;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.allOf;
+
+@LargeTest
+@RunWith(AndroidJUnit4.class)
+public class MainActivityTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityTestRule = new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void TextViewに表示されている文字列を検証するテストコードを書こう() {
+
+    }
+
+    @Test
+    public void CLICKとかかれたボタンを押してTextViewの文字列が変更するテストコードを書こう() {
+
+    }
+
+}
---
+++ helloespresso/src/main/java/android/shiroyama/us/helloespresso/MainActivity.java
@@ -0,0 +1,27 @@
+package android.shiroyama.us.helloespresso;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView textView;
+    private Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textView = (TextView) findViewById(R.id.text);
+        button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                textView.setText("CLICKED!");
+            }
+        });
+    }
+}
